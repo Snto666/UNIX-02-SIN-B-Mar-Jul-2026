d55dc3dd0efd99be4606f0152c493a020dbd722e
Snto666 <edyambayfa@uide.edu.ec>
2026-03-23 12:59:11 +0000

    New key export

--- Practical_workshop.sh
+++ Practical_workshop.sh
@@ -1,3 +1,10 @@
 uname -a
 which gpg
+gpg --full-generate-key
+gpg --list-keys // lsito todas las llaves que tengo
+gpg --armor --export santiagoyambay123@gmail.com > mi_llave_publica.asc
+gpg --armor-export //
 gpg --version
+gpg --list-secret-keys --keyid-format=long
+gpg --armor --export-secret-keys XXXXXXXXXX (FEBDE2535840B779)
+gpg --import llave_et.asc //(llave_et.asc=archivo donde se guardo la llave publica del compañero)
