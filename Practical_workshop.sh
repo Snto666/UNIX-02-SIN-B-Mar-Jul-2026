8d6f992124fbf0db9be653a2da50d65fd4be3f4e
Snto666 <edyambayfa@uide.edu.ec>
2026-03-23 13:25:50 +0000

    decrypt partner message

--- Practical_workshop.sh
+++ Practical_workshop.sh
@@ -10,4 +10,4 @@ gpg --armor --export-secret-keys XXXXXXXXXX (FEBDE2535840B779)
 gpg --import llave_et.asc //(llave_et.asc=archivo donde se guardo la llave publica del compañero)
 $ echo "ey ey ey pokkie, do not say that, you are beautiful" > doc_no_cifrado.txt // explicar que hace
 gpg --output doc_cifrado.txt --encrypt --recipient 3A07743689378D211DE90B9C2E2EAF7C82661FC2 doc_no_cifrado.txt //cifra el mensaje previamente guardado y lo envia encriptado hacia la llave de mi compañero
-
+gpg --decrypt estpu_doc_cifrado.txt // desencripta el mensaje enviao por mi compañero de manera que ya se puede leer su mensaje
