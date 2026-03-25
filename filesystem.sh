ee4892102c76b254630091a5e37ca1d8701e7a50
Snto666 <edyambayfa@uide.edu.ec>
2026-03-25 13:46:11 +0000

    Last commit of the class

--- filesystem.sh
+++ filesystem.sh
@@ -2,3 +2,5 @@ pwd #print working directory, command to display the full path of the current wo
 cd # It is used to change the current working directory .
 ls -F # A command to quickly identify the file type.
 ls -i # Show the file inode (something like the ID of the file)
+echo "hola" > test.txt 
+stat test.txt
