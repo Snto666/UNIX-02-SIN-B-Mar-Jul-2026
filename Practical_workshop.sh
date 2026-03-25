923c6e262419016fa5991430d554e72853006b50
Snto666 <edyambayfa@uide.edu.ec>
2026-03-25 12:11:35 +0000

    Change slash for commit 13

--- Practical_workshop.sh
+++ Practical_workshop.sh
@@ -10,5 +10,5 @@ gpg --armor --export-secret-keys XXXXXXXXXX (FEBDE2535840B779)
 gpg --import llave_et.asc //(llave_et.asc=archivo donde se guardo la llave publica del compañero)
 $ echo "ey ey ey pokkie, do not say that, you are beautiful" > doc_no_cifrado.txt // explicar que hace
 gpg --output doc_cifrado.txt --encrypt --recipient 3A07743689378D211DE90B9C2E2EAF7C82661FC2 doc_no_cifrado.txt //cifra el mensaje previamente guardado y lo envia encriptado hacia la llave de mi compañero
-gpg --decrypt estpu_doc_cifrado.txt // desencripta el mensaje enviao por mi compañero de manera que ya se puede leer su mensaje
+gpg --decrypt estpu_doc_cifrado.txt #desencripta el mensaje enviao por mi compañero de manera que ya se puede leer su mensaje
 gpg --output doc_no_cirfrado_firmado.txt --clearsign doc_no_cifrado.txt//Clear sign, no da binario  ni signos extraños, solo texto
