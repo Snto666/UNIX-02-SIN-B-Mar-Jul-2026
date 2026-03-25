2d1d8a3998bcb2df13120dd7413e547f21f84942
Snto666 <edyambayfa@uide.edu.ec>
2026-03-25 13:44:34 +0000

    Block and file test

--- filesystem.sh
+++ filesystem.sh
@@ -1,1 +1,4 @@
-pwd #print working directory, comando para mostrar la ruta completa del directorio de trabajo actual en Linux
+pwd #print working directory, command to display the full path of the current working directory in Linux
+cd # It is used to change the current working directory .
+ls -F # A command to quickly identify the file type.
+ls -i # Show the file inode (something like the ID of the file)
