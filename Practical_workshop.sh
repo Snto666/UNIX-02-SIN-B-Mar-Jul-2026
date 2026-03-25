55aa71a9d50b7c8d155c45d2c66ed6728420c488
Snto666 <edyambayfa@uide.edu.ec>
2026-03-25 18:53:34 +0000

    This sub-command allows you to set the level of confidence

--- Practical_workshop.sh
+++ Practical_workshop.sh
@@ -15,7 +15,7 @@ ls #Lists the files in my current directory.
 cat doc_no_cifrado.txt #Displays the content of the file directly in your terminal.
 gpg --verify santiago_doc_no_cifrado_firmado.txt #Verifies the digital signature of the file to confirm its authenticity and ensure it hasn't been tampered with.
 gpg --edit-key BF8A9258824D7DC2D83D51DBFEBDE2535840B779 #Opens an interactive menu to manage, edit, or sign the specific GPG key identified by that fingerprint.
-gpg> trust #This sub-command allows you to set the level of confidence you have in the key owner's ability to accurately validate other people's keys.
+gpg> trust #This sub-command allows you to set the level of confidence you have in the key owner's ability to accurately validate other people's keys..
 gpg --sign-key BF8A9258824D7DC2D83D51DBFEBDE2535840B779 #Use your private key to digitally sign the specified public key, officially certifying that you have verified the owner's identity.
 gpg --output doc_no_cifrado_firmado_binario.txt --sign doc_no_cifrado.txt #Creates a compressed binary file that contains both the original content and your digital signature.
 gpg --output doc_no_cifrado_firmado_binario.txt --sign doc_no_cifrado.txt #creates a compressed binary file that contains both the original content and your digital signature.
