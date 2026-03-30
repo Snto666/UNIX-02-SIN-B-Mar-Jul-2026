c911589136b64a13acde14a8370dfcd2a7361cc6
Snto666 <edyambayfa@uide.edu.ec>
2026-03-30 13:46:57 +0000

    Resume .txt

--- parted.sh
+++ parted.sh
@@ -4,3 +4,4 @@ sudo apt install parted # It searches for the program in the repositories and in
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" # It first shows the status of the physical disk with its partitions and then details the format of each one to know exactly what each disk is[ -d /sys/firmware/efi ] && echo ""UEFI" || echo "BIOS" # 
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #Check if there is a specific folder in the system to identify whether the computer boots in modern or old mode (BIOS or UEFI).
 echo "esto es un archivo" > archivo.txt # Create a text file called "file.txt" and write that phrase inside it.
+stat archivo.txt # Displays the entire file resume on a single screen
