2b75c1b771bca6ee1a51f78c56587e52cf46ccf8
Snto666 <edyambayfa@uide.edu.ec>
2026-03-30 13:45:18 +0000

    new text.txt

--- parted.sh
+++ parted.sh
@@ -3,3 +3,4 @@ sudo apt upgrade # Download and install the latest versions of all system progra
 sudo apt install parted # It searches for the program in the repositories and installs the partitioning tool automatically.
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" # It first shows the status of the physical disk with its partitions and then details the format of each one to know exactly what each disk is[ -d /sys/firmware/efi ] && echo ""UEFI" || echo "BIOS" # 
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #Check if there is a specific folder in the system to identify whether the computer boots in modern or old mode (BIOS or UEFI).
+echo "esto es un archivo" > archivo.txt # Create a text file called "file.txt" and write that phrase inside it.
