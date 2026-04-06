010af44f235eb0980762fecf8db2027eb8d59182
Snto666 <edyambayfa@uide.edu.ec>
2026-04-06 12:45:09 +0000

    cd list commits

--- script_boot.sh
+++ script_boot.sh
@@ -1,4 +1,5 @@
 cd #Change directory, changes the current working directory
-cd /home/codespace
-cd ~
-cd $HOME 
+cd /home/codespace #Use an absolute path to move the terminal session directly to the folder called codespace
+cd ~ #It performs and allows a fast and efficient movement towards the root
+cd $HOME  #It instantly transports the user to their root directory, regardless of their location.
+
