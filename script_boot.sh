6699483ae95273f12e2ffa231d80373fe5b74219
Snto666 <edyambayfa@uide.edu.ec>
2026-04-06 12:51:26 +0000

    Create new directory

--- script_boot.sh
+++ script_boot.sh
@@ -2,4 +2,8 @@ cd #Change directory, changes the current working directory
 cd /home/codespace #Use an absolute path to move the terminal session directly to the folder called codespace
 cd ~ #It performs and allows a fast and efficient movement towards the root
 cd $HOME  #It instantly transports the user to their root directory, regardless of their location.
-
+sudo mkdir proyecto #Create a new folder called "project" using administrator privileges to bypass any permission restrictions on the system.
+cd proyecto/ #Change the terminal's current working directory to place yourself within the "project" folder and begin working within it.
+ls -lai #Displays the detailed contents of the directory, including hidden files, their system identification numbers (inodes), and access permissions.
+925544 drwxr-xr-x 2 root root 4096 Apr  6 12:35 .
+714616 drwxr-xr-x 1 root root 4096 Apr  6 12:35 ..
