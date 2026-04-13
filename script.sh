816cb585ee56e2c466fabd49125c5ecd673b3496
Snto666 <edyambayfa@uide.edu.ec>
2026-04-13 13:00:17 +0000

    ls -lah commits

--- script.sh
+++ script.sh
@@ -7,3 +7,14 @@ ls --all
 ls -a /
 #Syntax: ls 
 #Explanation: -a / this command lists all files and directories located in the root directory.
+ls -l -a -h 
+#Syntax: ls
+#Explanation: -l -a -h Lists all files in a detailed long format with file sizes in humanreadable units (KB, MB, GB).
+ls -l -ah
+#Syntax: ls
+#Explanation: -l -ah It displays a list of all files in a long, detailed format, with file sizes in units.
+ls -lah
+#Syntax: ls
+##Explanation: -lah It displays a list of all files in a long, detailed format, with file sizes in units.
+mkdir -- -rf
+rmdir -- -rf
