b649a6290ecb7163baa438f677d99c419f611818
Snto666 <edyambayfa@uide.edu.ec>
2026-04-13 12:50:28 +0000

    Ls commits

---
+++ script.sh
@@ -0,0 +1,9 @@
+ls -a 
+# Syntax: ls
+#Explanation: -a tells the terminal to show all files, including hidden ones
+ls --all 
+#Syntax: ls
+#Explanation: --all tells the terminal to show all files, including hidden ones
+ls -a /
+#Syntax: ls 
+#Explanation: -a / this command lists all files and directories located in the root directory.
