ea5256ad371247166f39688819d94552a574e58a
Snto666 <edyambayfa@uide.edu.ec>
2026-04-15 13:08:32 +0000

    ./hola.sh give us a Permission denied

--- ejercicio_script.sh
+++ ejercicio_script.sh
@@ -1,3 +1,4 @@
 echo '#!/bin/sh' > hola.sh #Create a file called hello.sh and write the text '#!/bin/sh'
 echo 'echo "Hola desde mi primer script"' >> hola.sh #Add the text "Hello" from my first script to hola.sh without deleting the previous text
 cat hola.sh #Show the text inside hola.sh
+./hola.sh
