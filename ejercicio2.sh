e6ad55ff7aa81d226052833b595c6714f5cb32ac
Snto666 <edyambayfa@uide.edu.ec>
2026-04-15 13:40:42 +0000

    Displays detailed ile infomation

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,2 +1,3 @@
 touch prueba.txt #Create an empty file called "prueba.txt"
 chmod 600 prueba.txt #Change the permissions so that only the owner can read and write the file
+ls -l prueba.txt #It displays detailed file information, including its permissions, owner, size, and date.
