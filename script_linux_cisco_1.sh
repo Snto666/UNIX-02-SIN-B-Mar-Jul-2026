7d0230b725b19c26b41539dd54cb76dc4f545eb6
Snto666 <edyambayfa@uide.edu.ec>
2026-05-01 01:15:50 +0000

    Add documentation for "ls -l" command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -2,7 +2,7 @@ ls # Lists the files and directories in the current working directory
 ls Documents # Lists the contents specifically within the 'Documents' folder
 aptitude # A high-level interface for the package manager to install, remove, or update software
 aptitude moo # Triggers a hidden Easter egg that claims there are no Easter eggs in this program
-ls -l
+ls -l # Displays files using a long listing format, showing permissions, owner, and size
 ls -r
 ls -lr
 aptitude -v moo
