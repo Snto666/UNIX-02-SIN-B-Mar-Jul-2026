39c90b2473cc3641b8d990843f548d8cca5b49e9
Snto666 <edyambayfa@uide.edu.ec>
2026-05-01 01:16:19 +0000

    Add documentation for "ls -r" command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -3,8 +3,8 @@ ls Documents # Lists the contents specifically within the 'Documents' folder
 aptitude # A high-level interface for the package manager to install, remove, or update software
 aptitude moo # Triggers a hidden Easter egg that claims there are no Easter eggs in this program
 ls -l # Displays files using a long listing format, showing permissions, owner, and size
-ls -r
-ls -lr
+ls -r # Reverses the order of the directory listing
+ls -lr 
 aptitude -v moo
 aptitude -vv moo
 aptitude -vvv moo
