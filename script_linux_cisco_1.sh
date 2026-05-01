e7091c65c925172cc9fd3670ac296f21eaade8f1
Snto666 <edyambayfa@uide.edu.ec>
2026-05-01 05:01:33 +0000

    Add documentation for "./hello.sh" command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -40,7 +40,7 @@ ls -l hello.sh # Lists long format details for the specific file 'hello.sh'
 ./hello.sh # Attempts to execute the 'hello.sh' script in the current directory
 chmod u+x hello.sh # Grants execution permissions to the owner (user) of the file
 ls -l hello.sh # Lists long format details for the specific file 'hello.sh' 
-./hello.sh
+./hello.sh # Attempts to execute the 'hello.sh' script in the current directory
 #9) Cambiar el propietario de un archivo
 cd ~/Documents
 ls -l 
