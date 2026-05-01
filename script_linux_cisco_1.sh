437ab24adf0660d2a66774acec0639a1a816e552
Snto666 <edyambayfa@uide.edu.ec>
2026-05-01 03:54:46 +0000

    Add documentation for "pwd" command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -8,3 +8,22 @@ ls -lr # Lists files in long format and in reverse alphabetical order
 aptitude -v moo # Increases verbosity to get a different response from the hidden Easter egg
 aptitude -vv moo # Increases verbosity even further to continue the hidden dialogue
 aptitude -vvv moo # High verbosity that finally reveals the hidden ASCII art "cow"
+pwd # Displays the absolute path of the current working directory
+cd
+cd Documents  
+cd /
+cd /home/sysadmin
+cd School/Art
+cd ..   
+ls -l /var/log/
+ls -lt /var/log 
+ls -l -S /var/log 
+ls -lSr /var/log
+ls -r /var/log    
+su
+exit
+el siguiente ocmando es ocnfigurado de la sigiuente manera igual tienes que explicarlo (Para evitar ejecutar comandos sensibles o privilegiados, hemos configurado el comando steam locomotive, sl, para que requiera acceso administrativo. Si el comando se ejecuta como sysadmin, aparece un mensaje de error:
+
+sysadmin@localhost:~$ sl
+-bash: /usr/bin/sl: Permission denied)
+sudo sl
