d3fe529c60450e3eca5d5e6e072405c150f49a79
Snto666 <edyambayfa@uide.edu.ec>
2026-05-01 00:55:31 +0000

    Add documentation for "aptitude" command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,2 +1,3 @@
 ls # Lists the files and directories in the current working directory
 ls Documents # Lists the contents specifically within the 'Documents' folder
+aptitude # A high-level interface for the package manager to install, remove, or update software
