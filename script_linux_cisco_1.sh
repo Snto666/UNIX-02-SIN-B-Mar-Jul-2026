a9c65c2a97b2c5c06c75a1b72bfed863a6b5da6a
Snto666 <edyambayfa@uide.edu.ec>
2026-05-01 01:15:26 +0000

    Add documentation for "aptitude moo" command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,3 +1,10 @@
 ls # Lists the files and directories in the current working directory
 ls Documents # Lists the contents specifically within the 'Documents' folder
 aptitude # A high-level interface for the package manager to install, remove, or update software
+aptitude moo # Triggers a hidden Easter egg that claims there are no Easter eggs in this program
+ls -l
+ls -r
+ls -lr
+aptitude -v moo
+aptitude -vv moo
+aptitude -vvv moo
