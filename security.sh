b2eb7d1bca6f9e4085352171d16dbadf31031f56
Snto666 <edyambayfa@uide.edu.ec>
2026-05-04 13:37:07 +0000

    id -u command explication added

--- security.sh
+++ security.sh
@@ -2,3 +2,10 @@ id #Displays the current user's UID (User ID), GID (Group ID), and groups.
 cat /etc/passwd | head -10 # Reads the user account list and displays only the first 10 lines.
 groups # Lists all the groups the current user belongs to.
 groups $USER # Specifically queries and lists the groups for the username stored in the environment variable $USER
+id -u #Prints the effective user ID (numeric UID) only.
+id -g
+id -G
+cat /etc/group |  grep root
+cat /etc/gshadow
+mkdir ~/proyecto_unix/
+ls -la ~/proyecto_unix/
