8d39f7e213ebd6194133ccb55a15d7b978e23cfb
Snto666 <edyambayfa@uide.edu.ec>
2026-05-04 13:39:24 +0000

    cat /etc/group |  grep root command explication added

--- security.sh
+++ security.sh
@@ -5,7 +5,7 @@ groups $USER # Specifically queries and lists the groups for the username stored
 id -u #Prints the effective user ID (numeric UID) only.
 id -g # Prints the effective group ID (numeric GID) only.
 id -G # Prints all group IDs (supplementary GIDs) associated with the user.
-cat /etc/group |  grep root
+cat /etc/group |  grep root # Searches for the string "root" within the group definition file to show its GID and members.
 cat /etc/gshadow
 mkdir ~/proyecto_unix/
 ls -la ~/proyecto_unix/
