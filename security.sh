5799b83b13944095659c50309918af6fd688c0dc
Snto666 <edyambayfa@uide.edu.ec>
2026-05-04 13:27:28 +0000

    groups $USER command added

--- security.sh
+++ security.sh
@@ -1,3 +1,4 @@
 id #Displays the current user's UID (User ID), GID (Group ID), and groups.
 cat /etc/passwd | head -10 # Reads the user account list and displays only the first 10 lines.
 groups # Lists all the groups the current user belongs to.
+groups $USER # Specifically queries and lists the groups for the username stored in the environment variable $USER
