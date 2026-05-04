08c935c09e7305d2234c8a95fbf12a0b855abea2
Snto666 <edyambayfa@uide.edu.ec>
2026-05-04 13:37:30 +0000

    id -g ommand explication added

--- security.sh
+++ security.sh
@@ -3,7 +3,7 @@ cat /etc/passwd | head -10 # Reads the user account list and displays only the f
 groups # Lists all the groups the current user belongs to.
 groups $USER # Specifically queries and lists the groups for the username stored in the environment variable $USER
 id -u #Prints the effective user ID (numeric UID) only.
-id -g
+id -g # Prints the effective group ID (numeric GID) only.
 id -G
 cat /etc/group |  grep root
 cat /etc/gshadow
