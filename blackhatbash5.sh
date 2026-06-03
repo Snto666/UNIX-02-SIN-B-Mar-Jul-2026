c29a43cb9de2a6e12d83316d5d04a3ece93758f3
Snto666 <edyambayfa@uide.edu.ec>
2026-06-03 13:26:12 +0000

    Redirection Operators

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,18 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "el comando lzl fallo "
+
+echo "uno" > archivo.txt
+cat archivo.txt
+
+echo "uno-uno" > archivo.txt
+cat archivo.txt
+
+echo "uno-uno-dos" >> archivo.txt
+cat archivo.txt
+
+echo "tres" &> archivo.txt
+cat archivo.txt
+echo "tres" &>> archivo.txt
+cat archivo.txt
+
